4a5e5263a492fd29dfe79ecf36ee7f776b241656
Roma <dolgokakatel91@gmail.com>
2016-02-16 23:03:24 +0200

    Something.

--- main.lua
+++ main.lua
@@ -1,29 +1,94 @@
 local physics = require("physics")
-local screenWidth, screenHeght = display.contentWidth, display.contentHeight
-local redCollisionFilter = { categoryBits = 2, maskBits = 3 } -- collides with (2 & 1) only
-local blueCollisionFilter = { categoryBits = 4, maskBits = 5 } -- collides with (4 & 1) only
+--physics.setDrawMode("hybrid")
+local screenWidth, screenHeight = display.contentWidth, display.contentHeight
+local collisionFilter = { groupIndex = -1 }
 
-local redBody = { density=0.2, friction=0, bounce=0.95, radius=43.0, filter=redCollisionFilter }
-local blueBody = { density=0.2, friction=0, bounce=0.95, radius=43.0, filter=blueCollisionFilter }
-
-function pushObjectFromLeftSide()
-	local circle = display.newCircle(0, screenHeght/2, 10)
-	circle:setFillColor(0.3, 0.7, 0.9)
-	physics.addBody(circle, redBody)
-	circle:applyLinearImpulse( 6, -10, 0, 0 )
+function initBackground()
+	local backgtound = display.newRect(screenWidth/2, screenHeight/2, screenWidth, screenHeight)
+	backgtound:setFillColor(0.3, 0.2, 0.1)
 end
 
-function pushObjectFromRightSide()
-	local circle = display.newCircle(screenWidth, screenHeght/2, 10)
-	circle:setFillColor(0.9, 0.1, 0.9)
-	physics.addBody(circle, blueBody)
-	circle:applyLinearImpulse( -6, -10, 0, 0 )
+function initPlatform()
+	local platform1 = display.newRect(screenWidth/4, screenHeight*5/6, screenWidth/4, screenHeight/96)
+	physics.addBody(platform1, "static", {bounce = 0.3})
+	
+	local platform2 = display.newRect(screenWidth*3/4, screenHeight*5/6, screenWidth/4, screenHeight/96)
+	physics.addBody(platform2, "static", {bounce = 0.3})
+end
+
+function initObjectsPhysicOptions()
+	physicOptions = {density = 4, frictions = 0.1, bounce = 0.4,
+		filter = collisionFilter, radius = screenWidth/16}
+
+	objects = {}
+	objects[#objects + 1] = "res/nat.png"
+	objects[#objects + 1] = "res/bomb.png"
+end
+
+function initObjectImpulse()
+	leftImpulse = {}
+	leftImpulse.startCoordinate = leftStartCordinate
+	leftImpulse[1] = road1LeftImpulse
+	leftImpulse[2] = road2LeftImpulse
+	leftImpulse[3] = road3LeftImpulse
+	
+	rightImpulse = {}
+	rightImpulse.startCoordinate = rightStartCoordinate
+	rightImpulse[1] = road1RightImpulse
+	rightImpulse[2] = road2RightImpulse
+	rightImpulse[3] = road3RightImpulse
+	
+	impulse = {}
+	impulse[1] = leftImpulse
+	impulse[2] = rightImpulse
+end
+
+leftStartCordinate = function()
+	return 0, screenHeight/4
+end
+
+rightStartCoordinate = function()
+	return screenWidth, screenHeight/4
+end
+
+road1LeftImpulse = function()
+	return 170, -140, 0, 0
+end
+
+road2LeftImpulse = function()
+	return 300, 111, 0, 0
+end
+
+road1RightImpulse = function()
+	return -170, -140, 0, 0
+end
+
+road2RightImpulse = function()
+	return -300, 110, 0, 0
+end
+
+function pushObject()
+	local objects = objects[math.random(2)]
+	local impulseDirection = impulse[math.random(2)]
+	
+	local object = display.newImage(objects)
+	object.width = screenWidth/8
+	object.height = object.width
+	object.x, object.y = impulseDirection.startCoordinate()
+	
+	physics.addBody(object, physicOptions)
+	object:applyLinearImpulse(impulseDirection[math.random(2)]())
 end
 
 function main()
 	physics.start()
-	timer.performWithDelay(1000, pushObjectFromLeftSide, 0)
-	timer.performWithDelay(1000, pushObjectFromRightSide, 0)
+	physics.setScale(45)
+	initBackground()
+	initPlatform()
+	initObjectImpulse()
+	initObjectsPhysicOptions()
+	pushObject()
+	timer.performWithDelay(500, pushObject, 0)
 end
 
 --run main
